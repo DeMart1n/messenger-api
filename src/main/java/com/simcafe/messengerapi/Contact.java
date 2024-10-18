343a58a76292f6acff82d36b1bbae4b5061d447f
caua_de_martin <cauademartin@gmail.com>
2024-10-18 08:41:39 -0300

    Adição da classe Contact.java

--- src/main/java/com/simcafe/messengerapi/Contact.java
+++ src/main/java/com/simcafe/messengerapi/Contact.java
@@ -1,4 +1,50 @@
 package com.simcafe.messengerapi;
 
 public class Contact {
+
+    private String name;
+    private String phoneNumber;
+    private String message;
+
+    public String getName () {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber () {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber (String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getMessage () {
+        return message;
+    }
+
+    public void setMessage (String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString () {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    public Contact (String name, String phoneNumber, String message) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+
+
+
+    }
 }
